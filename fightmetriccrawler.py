cebde7c85f831bcc7bf940e6f73dced0d53cffd2
fungalcat <jimmyfrostedflakes@gmail.com>
2018-03-08 20:36:49 -0500

    Should be able to write now

--- fightmetriccrawler.py
+++ fightmetriccrawler.py
@@ -6,30 +6,30 @@ from random import randint
 source = requests.get('http://www.fightmetric.com/statistics/events/completed').text
 
 soup = BeautifulSoup(source, 'lxml')
-
-
-all_stats=[]
-#list of events
-links = soup.find_all('a',class_='b-link b-link_style_black')
-for link in links:
-	#individual event
-	source2 = requests.get(link['href']).text
-	soup2 = BeautifulSoup(source2, 'lxml')
-	links2 = soup2.find_all('a',class_='b-link b-link_style_black')
-	#list of fighters
-	for link2 in links2:
-		#individual fighter
-		source3 = requests.get(link2['href']).text
-		soup3 = BeautifulSoup(source3, 'lxml')
-		stats=[]
-		container3 = soup3.find_all('li',class_='b-list__box-list-item b-list__box-list-item_type_block')
-		for thing in container3:
-			stats.append(thing.text[27:].strip())
-		stats.pop(9)
-		print(stats)
-		all_stats.append(stats)
+import csv
+with open('fighters.csv', 'wb') as csvfile:
+	wr = csv.writer(csvfile, dialect='excel')
+	#list of events
+	links = soup.find_all('a',class_='b-link b-link_style_black')
+	for link in links:
+		#individual event
+		source2 = requests.get(link['href']).text
+		soup2 = BeautifulSoup(source2, 'lxml')
+		links2 = soup2.find_all('a',class_='b-link b-link_style_black')
+		#list of fighters
+		for link2 in links2:
+			#individual fighter
+			source3 = requests.get(link2['href']).text
+			soup3 = BeautifulSoup(source3, 'lxml')
+			stats=[]
+			container3 = soup3.find_all('li',class_='b-list__box-list-item b-list__box-list-item_type_block')
+			for thing in container3:
+				stats.append(thing.text[27:].strip())
+			stats.pop(9)
+			print(stats)
+			wr.writerow(stats)
+			time.sleep(randint(5,15))
 		time.sleep(randint(5,15))
-	time.sleep(randint(5,15))
 
 
 
