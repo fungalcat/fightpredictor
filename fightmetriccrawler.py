7a4c32cc7e2a20cf4463ce5f68ac584d1905bf8e
fungalcat <jimmyfrostedflakes@gmail.com>
2018-03-08 20:14:28 -0500

    1.1
    Crawler now starts on events page and crawls through events and fighters on events. Added wait timer.
    
    Need to add write to csv

--- fightmetriccrawler.py
+++ fightmetriccrawler.py
@@ -1,28 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-
-source = requests.get('http://www.fightmetric.com/event-details/7929be8290289a47').text
+import time
+from random import randint
+#events page
+source = requests.get('http://www.fightmetric.com/statistics/events/completed').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-body = soup.find('tbody')
-flag=0
-fights=[[],[]]
 
-#testing
 all_stats=[]
-links = body.find_all('a',class_='b-link b-link_style_black')
+#list of events
+links = soup.find_all('a',class_='b-link b-link_style_black')
 for link in links:
+	#individual event
 	source2 = requests.get(link['href']).text
 	soup2 = BeautifulSoup(source2, 'lxml')
-	container = soup2.find('div',class_='b-list__info-box b-list__info-box_style_small-width js-guide')
-	stats=[]
-	container = soup2.find_all('li',class_='b-list__box-list-item b-list__box-list-item_type_block')
-	for thing in container:
-		stats.append(thing.text[27:].strip())
-	stats.pop(9)
-	print(stats)
-	all_stats.append(stats)
+	links2 = soup2.find_all('a',class_='b-link b-link_style_black')
+	#list of fighters
+	for link2 in links2:
+		#individual fighter
+		source3 = requests.get(link2['href']).text
+		soup3 = BeautifulSoup(source3, 'lxml')
+		stats=[]
+		container3 = soup3.find_all('li',class_='b-list__box-list-item b-list__box-list-item_type_block')
+		for thing in container3:
+			stats.append(thing.text[27:].strip())
+		stats.pop(9)
+		print(stats)
+		all_stats.append(stats)
+		time.sleep(randint(5,15))
+	time.sleep(randint(5,15))
 
 
 
